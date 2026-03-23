454bd42de353b385c1e6a847b9c2475956650dd8
mkielak <kielakowsky17@gmail.com>
2026-03-23 12:22:48 +0100

    Fixed to use testing db for tests using h2

--- backend/src/test/java/com/kielakjr/sports_events/SportsEventsApplicationTests.java
+++ backend/src/test/java/com/kielakjr/sports_events/SportsEventsApplicationTests.java
@@ -2,8 +2,10 @@ package com.kielakjr.sports_events;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SportsEventsApplicationTests {
 
 	@Test
